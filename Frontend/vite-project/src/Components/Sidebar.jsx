118b531ec490a9baa01f3c39f0c0c752940b786b
SHUBHANK MAHESHWARI <maheshwarishubhank2002@gmail.com>
2025-08-16 18:48:58 +0530

    added sidebar

--- Frontend/vite-project/src/Components/Sidebar.jsx
+++ Frontend/vite-project/src/Components/Sidebar.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useState } from "react";
 const Sidebar = () => {
     const [Company_list, setCompany_list] = useState([])
+    const [selected, setselected] = useState(null)
     useEffect(()=>{
         GET_Company_List()
     },[])
@@ -22,12 +23,12 @@ const Sidebar = () => {
         return <div className="text-center w-2xl mx-auto">Loading...</div>
     }
   return (
-    <div className="w-[25vw] flex flex-col h-screen">
-      <div>📈 Stock Dashboard</div>
-      <ul className="list-none flex flex-col gap-3">
+    <div className="w-[25vw] flex flex-col text-gray-200 shadow-lg bg-gray-800 min-h-screen gap-18 pt-10 px-5">
+      <div className="text-2xl font-bold text-green-400 border-b border-gray-800 p-3">📈 Stock Dashboard</div>
+      <ul className="list-none flex flex-col gap-15 overflow-auto mb-10 items-center ">
         {Company_list && (
             Company_list.map((item,index)=>{
-                return <li key={index}>{item.Symbol}</li>
+                return <li key={index} onClick={()=>setselected(item.Symbol)} className={`py-3 font-bold px-15 rounded-md cursor-pointer ${selected===item.Symbol? "bg-red-400 text-white":"hover:bg-gray-300 hover:text-white"} transition-colors duration-200`}>{item.Symbol}</li>
             })
         )}
       </ul>
